572d4cf1ddf24e7bc97fd4f3391a2fb7ed3e0a10
Zach <zach.stevensonwright@student.onslow.school.nz>
2026-06-22 22:00:14 +0000

    Done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,57 +5,15 @@
 struct SwiftPlayground {
     static func main() {
 
- // Introduction to Zealandia tracker
-print("Welcome to Zealandia tracker")
+var number: [Int] = [1,2,3,4,5]
+print("Welcome to screen time tracker!")
+// Tells user they are using screen time tracker
 
-/// Shows us a list of birds and insects Maia has seen. 
-let birdList: [String] = ["Tieke", "Kākā","Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī","Kererū"]
-let insectList: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko","Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
-var speciesMaiaSeen: [String] = []
-var numberOfSpecies 
-var isRunning = true  
+print("What is the hour limit set for each day?")
+print("Enter a number 1-5:")
+if var numberHour <= 5 {
 
-
-/// Asks if you have seen a bird or a insect 
-/// Gives you a list of birds that you have seen
-print("Did you see a bird or insect?")
-let birdOrInsect = readLine()
-if birdOrInsect?.lowercased() == "bird" {
-    print("Which bird did you see?")
-    birdList.enumerated().forEach{ Index, birdName in 
-    print("\(Index+1).\(birdName)") }
-    print("Enter a Number")
-let numberSelected: Int = Int(readLine()!)!
-let birdMaiaSaw = birdList[numberSelected - 1]
-speciesMaiaSeen.append(birdMaiaSaw)
-print("Added a \(birdMaiaSaw)")
-
-if speciesMaiaSeen.contains(birdMaiaSaw){
-    let Index = speciesMaiaSeen.firstIndex(of:birdMaiaSaw)! 
-    numberOfSpecies[Index] = numberOfSpecies[Index]+1
 }
-    }
-
-/// Asks if you have seen a bird or a insect 
-/// Gives you a list of insects that you have seen
-if birdOrInsect?.lowercased() == "insect" {
-    print("Which insect did you see?")
-    insectList.enumerated().forEach { Index, insectName in 
-    print("\(Index+1).\(insectName)") }
-    print("Enter a number")
-let numberSelected: Int = Int(readLine()!)!
-let insectMaiaSaw = insectList[numberSelected - 1]
-speciesMaiaSeen.append(insectMaiaSaw)
-print("Added a \(insectMaiaSaw)")
-}
-
-
-print("Do you want contuine Y/N ")
-if Y = true 
-else if N == 
-isRunning == false
-
-
 
 
     }
