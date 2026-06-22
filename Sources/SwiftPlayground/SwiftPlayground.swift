19a54d977a16419d0675d0182af7b0936edfb76a
Zach <zach.stevensonwright@student.onslow.school.nz>
2026-06-22 22:04:36 +0000

    d

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,13 +5,16 @@
 struct SwiftPlayground {
     static func main() {
 
-var number: [Int] = [1,2,3,4,5]
 print("Welcome to screen time tracker!")
 // Tells user they are using screen time tracker
 
 print("What is the hour limit set for each day?")
 print("Enter a number 1-5:")
-let numberOfHours = readLine()
+let number = readLine()
+if numberOfHours: [Int] <= 5 {
+
+}
+
 
 }
 
