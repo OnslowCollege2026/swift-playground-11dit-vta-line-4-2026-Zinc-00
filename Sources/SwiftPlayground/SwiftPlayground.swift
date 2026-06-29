385a71453dd81b31ed9731c89afeb01ee7e03f49
Zach <zach.stevensonwright@student.onslow.school.nz>
2026-06-29 22:00:51 +0000

    Jun 29, 2026, 10:00 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -10,7 +10,7 @@ var isRunning =  true
 var isRunning2 = true
 let apps: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "Other"]
 let hours: Double = 0.00
-let totalUsage: Double = 0.0
+
 
 var appUsage: [String: String] = [:]
 
@@ -30,15 +30,15 @@ var totalUsage: Int = 0
 for app in apps {
     print("How many hours did you spend on \(app)?")
 
-    if let input = readLine() {  let inputInt = Double(input) ?? 0
-
-if inputInt >= 0.00 && inputInt <= 24.00 {
-    totalUsage += Int(inputInt)
-    appUsage[app] = input
-} else {
-    print("Out of bounds please put in your correct screen time.")
-}
-
+    if let input = readLine() { 
+        let totalUsage: Double
+        let inputInt = Double(input) ?? 0
+        appUsage[app] = input
+        if let hours = Double(input) {
+        totalUsage += hours
+        
+        if inputInt < 0.00 || inputInt > 24.00 {
+            print("Out of bounds please use put in your correct screen time.")
         } else { 
 print("Your total usage time of all apps for Monday is \(totalUsage)")
         }
