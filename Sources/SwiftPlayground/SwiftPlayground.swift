5f666e66de0920810a272d98274a09c12c3ca91a
Zach <zach.stevensonwright@student.onslow.school.nz>
2026-06-29 22:04:11 +0000

    Jun 29, 2026, 10:04 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -39,17 +39,15 @@ for app in apps {
         
         if inputInt < 0.00 || inputInt > 24.00 {
             print("Out of bounds please use put in your correct screen time.")
-    
         }
         } else { 
     print("Please enter a number.")
-    
-print("Your total usage time of all apps for Monday is \(totalUsage)")
 
     }
 }
 
 }
+print("Your total usage time of all apps for Monday is \(totalUsage)")
 
 
 
